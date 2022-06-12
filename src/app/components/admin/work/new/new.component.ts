22ab0d15ed3b876459c6092fcbd968d710e461f0
Nhật Anh <92773027+nnanh19@users.noreply.github.com>
2022-06-12 19:21:00 +0700

    works crud

--- src/app/components/admin/work/new/new.component.ts
+++ src/app/components/admin/work/new/new.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { WorkService } from 'src/app/service/work.service';
 @Component({
   selector: 'app-new',
   templateUrl: './new.component.html',
   styleUrls: ['./new.component.css']
 })
 export class NewComponent implements OnInit {
+  validateForm!: FormGroup;
+  submitForm(): void {
+    if (this.validateForm.valid) {
+          this.workService.create(this.validateForm.value).subscribe(() => {
+            this.notification.create(
+              'success',
+              'Thêm mới thành công',
+              ''
+            )
+            this.router.navigateByUrl('/admin/work')
+          })
+    } else {
+      Object.values(this.validateForm.controls).forEach(control => {
+        if (control.invalid) {
+          control.markAsDirty();
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
 
-  constructor() { }
+
+  confirmationValidator = (control: FormControl): { [s: string]: boolean } => {
+    if (!control.value) {
+      return { required: true };
+    }
+    return {};
+  };
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private workService: WorkService,
+    private activateRoute : ActivatedRoute,
+    private notification: NzNotificationService
+    ) {
+     
+    }
 
   ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      img: [null, [Validators.required]],
+      title: [null, [Validators.required]],
+      genre: [null, [Validators.required]],
+      frameworks: [null, [Validators.required]],
+      link_git: [null, [Validators.required]],
+      link_web: [null],
+      createdAt: [null, [Validators.required]],
+    });
+   
   }
 
 }
